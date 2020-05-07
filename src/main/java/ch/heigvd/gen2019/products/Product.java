0a56d6f7f28ec10fd5712855837eb9904458676b
pabloheigvd <pabloheigvd@gmail.com>
2020-05-07 12:50:19 +0200

    refactoring/move-method getColorFor() de OrdersWriter -> Product

---
+++ src/main/java/ch/heigvd/gen2019/products/Product.java
@@ -0,0 +1,70 @@
+package ch.heigvd.gen2019.products;
+
+public class Product {
+    public static final int SIZE_NOT_APPLICABLE = -1;
+    private String code;
+    private int color;
+    private int size;
+    private double price;
+    private String currency;
+
+    public Product(String code, int color, int size, double price, String currency) {
+        this.code = code;
+        this.color = color;
+        this.size = size;
+        this.price = price;
+        this.currency = currency;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public String getSizeFor() {
+        switch (getSize()) {
+            case 1:
+                return "XS";
+            case 2:
+                return "S";
+            case 3:
+                return "M";
+            case 4:
+                return "L";
+            case 5:
+                return "XL";
+            case 6:
+                return "XXL";
+            default:
+                return "Invalid Size";
+        }
+    }
+
+    public String getColorFor() {
+        switch (getColor()) {
+            case 1:
+                return "blue";
+            case 2:
+                return "red";
+            case 3:
+                return "yellow";
+            default:
+                return "no color";
+        }
+    }
+}
